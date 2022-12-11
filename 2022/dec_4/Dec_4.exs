fd4792e92d46b5aa69d6a59c0e729b5c7829cdec
lassebloch <lassebloch@gmail.com>
2022-12-11 07:58:01 +0100

    🎄

--- 2022/dec_4/Dec_4.exs
+++ 2022/dec_4/Dec_4.exs
@@ -3,15 +3,16 @@ defmodule Dec4 do
     input
     |> String.split("\n", trim: true)
     |> Enum.flat_map(&String.split(&1, ","))
-    |> String.split("-")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&String.split(&1, "-"))
+    |> Enum.map(fn list -> Enum.map(list, &String.to_integer/1) end)
     |> Enum.chunk_every(2)
   end
 
-  def total_overloap?(pairs) do
+  defp total_overloap?(pairs) do
     [elf_1, elf_2] = pairs
 
-    # If we sort the egdes, if the value for an elf is at the extrems the other must be inccluded in that
+    # If we find the extremes of the elf lists combined,  and one of the elf list is equal to that
+    # the otherlist must be included in that 
     extremes = Enum.min_max(elf_1 ++ elf_2) |> Tuple.to_list()
 
     cond do
@@ -21,10 +22,29 @@ defmodule Dec4 do
     end
   end
 
+  defp any_overloap?(pairs) do
+    [elf_1, elf_2] = pairs
+    {elf_1_s, elf_1_e} = elf_1 |> List.to_tuple()
+    {elf_2_s, elf_2_e} = elf_2 |> List.to_tuple()
+
+    cond do
+      elf_1_s <= elf_2_s && elf_2_s <= elf_1_e -> true
+      elf_2_s <= elf_1_s && elf_1_s <= elf_2_e -> true
+      true -> false
+    end
+  end
+
   def part_1(assignments) do
     assignments
     |> get_pairs_from_input
     |> Enum.count(&total_overloap?/1)
+    |> IO.inspect()
+  end
+
+  def part_2(assignments) do
+    assignments
+    |> get_pairs_from_input
+    |> Enum.count(&any_overloap?/1)
     |> IO.inspect()
   end
 end
@@ -41,3 +61,5 @@ test_input = """
 Dec4.part_1(test_input) |> IO.inspect(label: "part 1 test data")
 {:ok, raw_data} = File.read("input.txt")
 Dec4.part_1(raw_data) |> IO.inspect(label: "part 1 real data")
+Dec4.part_2(test_input) |> IO.inspect(label: "part 2 test data")
+Dec4.part_2(raw_data) |> IO.inspect(label: "part 2 real data")
